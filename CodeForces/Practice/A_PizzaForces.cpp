2e87d76f8e882c8ce77df165678129ccddbbe526
Shikhar03Stark <hv.harshit321@gmail.com>
2022-02-20 17:30:01 +0530

    Code dump 20/02/2022

---
+++ CodeForces/Practice/A_PizzaForces.cpp
@@ -0,0 +1,31 @@
+// Author: Harshit Vishwakarma (Shikhar03Stark)
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define nl '\n'
+#define ll long long int
+#define ull unsigned long long int
+#define dev(x) cerr << #x << " " << x << endl
+using namespace std;
+
+void pre(){
+    
+    return;
+}
+
+void solve(){
+    ll n;
+    cin >> n;
+    cout << max(6LL, n+1)/2 * 5LL << nl;
+    return;
+}
+
+int main(){
+    FASTIO
+    pre();
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
